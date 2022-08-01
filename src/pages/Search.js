8c75eb7a61e0afe505532084a07327dfccbc099a
Vinicius Kaminski <vikaminski@hotmail.com>
2022-08-01 19:33:16 -0300

    Requisito 4 ao 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,8 +2,49 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      artistValue: '',
+    };
+  }
+
+  onChange = ({ target }) => {
+    this.setState({
+      artistValue: target.value });
+  }
+
+  artistLength = () => {
+    const nameLength = 2;
+    const { artistValue } = this.state;
+    return artistValue.length < nameLength;
+  }
+
   render() {
-    return (<div data-testid="page-search"><Header /></div>);
+    const { artistValue } = this.state;
+    return (
+      <div data-testid="page-search">
+        <Header />
+        <form>
+          <label htmlFor="artistBand">
+            <input
+              name="artistBand"
+              value={ artistValue }
+              data-testid="search-artist-input"
+              type="text"
+              onChange={ this.onChange }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ this.artistLength() }
+          >
+            Pesquisar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
