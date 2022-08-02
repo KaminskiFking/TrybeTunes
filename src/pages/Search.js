5c708c96d72da0b2b2faf5ea4759355e177ba720
Vinicius Kaminski <vikaminski@hotmail.com>
2022-08-01 23:26:21 -0300

    Requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Header from './Header';
+import Loading from './Loading';
 
 class Search extends Component {
   constructor() {
     super();
     this.state = {
       artistValue: '',
+      loading: '',
+      nameArtist: '',
+      album: [],
     };
   }
 
   onChange = ({ target }) => {
     this.setState({
-      artistValue: target.value });
+      artistValue: target.value,
+    });
   }
 
   artistLength = () => {
@@ -20,8 +27,29 @@ class Search extends Component {
     return artistValue.length < nameLength;
   }
 
+  clickPesquisar = async (event) => {
+    event.preventDefault();
+    const {
+      artistValue,
+    } = this.state;
+    this.setState({
+      loading: true,
+    });
+    const test = await searchAlbumsAPI(artistValue);
+    console.log(test);
+    this.setState((prevState) => ({
+      nameArtist: `Resultado de álbuns de: ${prevState.artistValue}`,
+      artistValue: '',
+      loading: false,
+    }));
+    this.setState({
+      album: test,
+    });
+  }
+
   render() {
-    const { artistValue } = this.state;
+    const { artistValue, loading, nameArtist, album } = this.state;
+    console.log(album);
     return (
       <div data-testid="page-search">
         <Header />
@@ -35,14 +63,35 @@ class Search extends Component {
               onChange={ this.onChange }
             />
           </label>
-          <button
-            data-testid="search-artist-button"
-            type="button"
-            disabled={ this.artistLength() }
-          >
-            Pesquisar
-          </button>
+          { loading ? (<Loading />) : (
+            <button
+              data-testid="search-artist-button"
+              type="button"
+              disabled={ this.artistLength() }
+              onClick={ this.clickPesquisar }
+            >
+              Pesquisar
+            </button>)}
         </form>
+        {album.length < 1
+          ? <p>Nenhum álbum foi encontrado</p> : album.map((element, index) => (
+            <div key={ index }>
+              { element.artistId }
+              { element.artistName }
+              { element.collectionId }
+              { element.collectionName }
+              { element.collectionPrice }
+              { element.artworkUrl100 }
+              { element.releaseDate }
+              { element.trackCount }
+              <Link
+                to={ `/album/${element.collectionId}` }
+                data-testid={ `link-to-album-${element.collectionId}` }
+              >
+                Album Link
+              </Link>
+            </div>))}
+        {nameArtist}
       </div>
     );
   }
